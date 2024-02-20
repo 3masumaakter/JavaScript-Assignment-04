ac40fb21430462be1a1435553f1eab5f23a53698
unknown <masumaakter.a1@gmail.com>
2024-02-20 14:24:00 +0600

    first commit

---
+++ else if.js
@@ -0,0 +1,17 @@
+
+var coffePrice = 50;
+var teaPrice = 20;
+var lichiPrice = 10;
+var myBudget = 41;
+
+if(coffePrice > myBudget){
+    console.log("give me a coffe");
+}
+
+else if(lichiPrice < myBudget){
+    console.log("give me a lichi");
+}
+else{
+    console.log("give me a tea");
+}
+
---
+++ if-else.js
@@ -0,0 +1,11 @@
+var coffePrice = true;
+var tea = true;
+
+
+if (coffePrice == true){
+    console.log('give me coffe!!');
+}
+
+else{
+    console.log('give me a tea!!')
+}
